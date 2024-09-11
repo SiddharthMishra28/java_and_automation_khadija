3d74a06dc2d17440438935f631bc3a17fd52e4e2
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2024-09-11 21:50:22 +0530

    added object collection examples

---
+++ src/com/java/collections/Address.java
@@ -0,0 +1,55 @@
+package com.java.collections;
+
+public class Address {
+	
+	private int id;
+	private String houseNo;
+	private String street;
+	private String city;
+	private String state;
+	private String country;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", houseNo=" + houseNo + ", street=" + street + ", city=" + city + ", state="
+				+ state + ", country=" + country + "]";
+	}
+	
+}
